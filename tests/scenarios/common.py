ce5a3cf224736d9cdc2e03f7d799318e6edf4fcc
jmoore <josh@glencoesoftware.com>
2014-04-17 18:31:36 +0200

    Begin work on PR scenario

---
+++ tests/scenarios/common.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+:author: Josh Moore <josh@glencoesoftware.com>
+
+Test for Corgi
+Copyright (C) 2014 Glencoe Software, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+import os
+import json
+
+from corgi_loves import bark_corgi_bark
+from corgi_loves import register_corgis
+from corgi_loves import INITIALIZED
+
+from blinker import signal
+from configobj import ConfigObj
+
+
+class Base(object):
+
+    HANLDERS = []
+
+    def __init__(self):
+        self.configfile = os.path.join(os.path.dirname(__file__), '..', '..', 'server.cfg')
+        self.datadir = os.path.join(os.path.dirname(__file__), 'data')
+        self.config = ConfigObj(self.configfile, interpolation=False, file_error=True)
+
+        bark_corgi_bark(self.config)
+        self.instances = register_corgis(self.HANDLERS, debug=True)
+
+        INITIALIZED.send(self.__class__.__name__, paths={})
+
+    def main(self):
+        raise Exception("must be implemented")
